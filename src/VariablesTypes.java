2bcb219957bf84a1031935db1e05bc0131c35c4e
Gustavo Santana <gsb.jmt@gmail.com>
2025-03-25 09:51:21 -0400

    initial commit

---
+++ src/VariablesTypes.java
@@ -0,0 +1,15 @@
+public class VariablesTypes {
+    public static void main(String[] args) {
+
+        //1 tipos primitivos
+        double minSalary = 1600.50;
+        byte age = 19;
+        short year = 2025;
+        float coeficiente = 8.8F;
+        long cpf = 1234567890L;
+        int projectsActive = 121;
+
+        //1.1 constante
+        final double PI_VALUE = 3.14;
+    }
+}
